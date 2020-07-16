55c2d9260da2fd5a2f6bdab4dfb43f10fff3823d
Jacob Garcia <youremail@example.com>
2020-07-16 18:51:57 -0400

    Created the opening dialogue

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
-int main() {
+using namespace std;
 
+int main() {
+	cout << "Calculator Console Application\n";
+	cout << "Please enter the following operation to perform. Format: a+b | a-b | a/b | a*b\n";
+	return 0;
 }
